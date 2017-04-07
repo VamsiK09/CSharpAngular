b2fb765ecb42d1100bc850ae9c5182b415e81b02
Vamsi Kolakani <vamckolakani@gmail.com>
2017-04-06 20:59:50 -0400

    added inserts for job and locations

--- JobApplication/JobApplication/Controllers/HomeController.cs
+++ JobApplication/JobApplication/Controllers/HomeController.cs
@@ -40,16 +40,41 @@ namespace JobApplication.Controllers
 
         public JsonResult GetStatus()
         {
-            //List<JobStatu> statusList = new List<JobStatu>();
-            //statusList.Add(new JobStatu { Name = "Applied" });
-            //statusList.Add(new JobStatu { Name = "Accepted" });
-            //statusList.Add(new JobStatu { Name = "Rejected" });
-            //statusList.Add(new JobStatu { Name = "NoResponse" });
-            //statusList.Add(new JobStatu { Name = "InProgress" });
-            //model.JobStatus.AddRange(statusList);
+            List<JobStatu> statusList = new List<JobStatu>();
+            statusList.Add(new JobStatu { Name = "Applied" });
+            statusList.Add(new JobStatu { Name = "Accepted" });
+            statusList.Add(new JobStatu { Name = "Rejected" });
+            statusList.Add(new JobStatu { Name = "NoResponse" });
+            statusList.Add(new JobStatu { Name = "InProgress" });
+            model.JobStatus.AddRange(statusList);
             //int result = model.SaveChanges();
             var status = model.JobStatus.ToList();
             return Json(status, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetJob()
+        {
+            List<Job> JobList = new List<Job>();
+            JobList.Add(new Job { Position = "Software Engineer" , CompanyName ="Amazon", LinkedInContact ="Susan" ,  });
+            JobList.Add(new Job { Position = "Software Developer", CompanyName ="Google", LinkedInContact = "Vamsi" });
+            model.Jobs.AddRange(JobList);
+            int result = model.SaveChanges();
+            var status = model.Jobs.ToList();
+            return Json(status, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetPlace() 
+        {
+            List<JobLocation> Location = new List<JobLocation>();
+            Location.Add(new JobLocation { City = "Nashua", State = "NewHampshire", Country = "USA" });
+            Location.Add(new JobLocation { City = "Edison", State = "New Jersey", Country = "USA" });
+            Location.Add(new JobLocation { City = "Mass", State = "Massachusetts", Country = "USA" });
+            Location.Add(new JobLocation { City = "Dayton", State = "Texas", Country = "USA" });
+            model.JobLocations.AddRange(Location);
+            int result = model.SaveChanges();
+            var status = model.JobLocations.ToList();
+            return Json(status, JsonRequestBehavior.AllowGet); 
+
         }
     }
 }
